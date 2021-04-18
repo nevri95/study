98db24d7c395572dd41123bdf21af921f64acede
nevri <784834414@qq.com>
2021-04-18 17:58:25 +0800

    add project

---
+++ src/main/java/common/publicConfig/PublicParameter.java
@@ -0,0 +1,14 @@
+package common.publicConfig;
+
+/**
+ * @author Nevri on 2021/4/13
+ * @Title:
+ * @Description:用于存放公共的配置项
+ */
+public class PublicParameter {
+    public static final  String   PROPERTIES_FILE ="parameter.properties";
+
+    public static final  String   BROSWER_TYPE ="broswerType";
+    public static final  String   TEST_URL ="testUrl";
+
+}
